07b2236e8d42ef5128de06d5f04bd835b67bc480
Long Bui <bnlong@fossil.com>
2020-02-28 07:16:42 +0700

    Benchmar Chi and Fiber only.

--- libs.sh
+++ libs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-web_frameworks=("default" "atreugo" "beego" "bone" "chi" "denco" "echov3" "fasthttp-raw" \
-"fasthttp-routing" "fasthttp/router" "fasthttprouter" "fastrouter" "fiber" "fresh" "gear" "gin" "goji" "gojsonrest" "gongular" "gorestful" "gorilla" "gorouter" "gorouterfasthttp" "go-ozzo" "gowww" "gramework" "httprouter" \
-"httptreemux" "lars" "lion" "martini" "muxie" "negroni" "neo" "pat" "pure" "r2router" "tango" "tiger" "tinyrouter" "traffic" "violetear" "vulcan" "webgo")
+# web_frameworks=("default" "atreugo" "beego" "bone" "chi" "denco" "echov3" "fasthttp-raw" \
+# "fasthttp-routing" "fasthttp/router" "fasthttprouter" "fastrouter" "fiber" "fresh" "gear" "gin" "goji" "gojsonrest" "gongular" "gorestful" "gorilla" "gorouter" "gorouterfasthttp" "go-ozzo" "gowww" "gramework" "httprouter" \
+# "httptreemux" "lars" "lion" "martini" "muxie" "negroni" "neo" "pat" "pure" "r2router" "tango" "tiger" "tinyrouter" "traffic" "violetear" "vulcan" "webgo")
+
+web_frameworks=("chi" "fiber")
